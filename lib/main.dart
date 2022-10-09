128db33d236f7c8d4339d6ca2873c467e164d3d7
AkYazilimDestek <111167457+AkYazilimDestek@users.noreply.github.com>
2022-10-09 09:14:06 +0300

    son değişiklik

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+
 void main() => runApp(App());
 
 class App extends StatelessWidget {
@@ -12,30 +13,30 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home : _Page(),
+      home : _Vucut(),
     );
   }
 }
 
-class _Page extends StatefulWidget {
-  const _Page({Key? key}) : super(key: key);
+class _Vucut extends StatefulWidget {
+  const _Vucut({Key? key}) : super(key: key);
 
   @override
-  State<_Page> createState() => _PageState();
+  State<_Vucut> createState() => _VucutState();
 }
 
-class _PageState extends State<_Page> {
+class _VucutState extends State<_Vucut> {
+
   late List imageList;
-  bool loading = true;
+  late bool loading = true;
   fetchAllImage() async {
-    final response  = await http.get(Uri.parse("https://raw.githubusercontent.com/AkYazilimDestek/dynamik_slider-flutter/b1b8de6f9755f855ea87bfac47ea3bbf920a6388/slider.json"));
+    final response = await http.get(Uri.parse("https://raw.githubusercontent.com/AkYazilimDestek/dynamik_slider-flutter/b1b8de6f9755f855ea87bfac47ea3bbf920a6388/slider.json"));
     if (response.statusCode == 200) {
       setState(() {
-          imageList = jsonDecode(response.body);
-          loading = false;
+        imageList = jsonDecode(response.body);
+        loading = false;
       });
     }
-
   }
 
   @override
@@ -48,21 +49,23 @@ class _PageState extends State<_Page> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("flutter slider"),
+        title: Text("Dynamic slider"),
       ),
-      body : Center(
-        child : Column(
-          children: [
-            SizedBox(
-                height: 150.0,
-                width: 300.0,
-                child: Carousel(
+      body : Container(
+        child: Center(
+          child : Column(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                height: 200,
+                child :  loading? CircularProgressIndicator() : Carousel(
                   images: imageList.map((element) {
-                    return Image.network(element["image"].toString());
+                    return Image.network(element["image"]);
                   }).toList(),
-                )
-            )
-          ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
